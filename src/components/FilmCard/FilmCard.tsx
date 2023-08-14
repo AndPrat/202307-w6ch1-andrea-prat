43db645b900ff94725a49651e6259ec24eed87c6
Andrea Prat <andr8ide@gmail.com>
2023-08-14 18:01:40 +0200

    Modify FilmCard with Props

--- src/components/FilmCard/FilmCard.tsx
+++ src/components/FilmCard/FilmCard.tsx
@@ -1,19 +1,29 @@
+import React from "react";
 import { X } from "react-feather";
+import { Film } from "../../types";
 import Button from "../Button/Button";
 import "./FilmCard.css";
 
-const FilmCard = (): React.ReactElement => {
+interface FilmCardStructureProps {
+  film: Film;
+}
+
+const FilmCard = ({
+  film: { director, title, year, poster },
+}: FilmCardStructureProps): React.ReactElement => {
   return (
     <article className="film">
-      <h3>Viaje a Darjeeling (2018)</h3>
+      <h3>
+        {title} ({year})
+      </h3>
       <img
         className="film__poster"
-        src="https://es.web.img3.acsta.net/c_310_420/medias/nmedia/18/67/28/56/20350733.jpg"
+        src={poster}
         alt="Cartel de 'Viaje a Darjeeling'"
         width="300"
         height="406"
       />
-      Dirección: Wes Anderson
+      <span>Dirección: {director}</span>
       <Button className="film__button">
         <X aria-label="eliminar película" />
       </Button>
