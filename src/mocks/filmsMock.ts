fdaab59230e3a05f96638e69601dbfce45019508
Andrea Prat <andr8ide@gmail.com>
2023-08-14 18:00:34 +0200

    Create data.tsx and filmsMock.ts for test FilmCard.tsx

---
+++ src/mocks/filmsMock.ts
@@ -0,0 +1,10 @@
+import { Film } from "../types";
+
+export const filmMock: Film = {
+  id: 1,
+  title: "Viaje a Darjeeling",
+  poster:
+    "https://es.web.img3.acsta.net/c_310_420/medias/nmedia/18/67/28/56/20350733.jpg",
+  director: "Wes Anderson",
+  year: 2007,
+};
