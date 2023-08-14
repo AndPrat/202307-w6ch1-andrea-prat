bddcbdd837a22d5d392198ae3635a9e956586f2d
Andrea Prat <andr8ide@gmail.com>
2023-08-14 18:38:21 +0200

    Modify FilmList with Props

--- src/components/FilmsList/FilmsList.tsx
+++ src/components/FilmsList/FilmsList.tsx
@@ -1,3 +1,4 @@
+import filmsData from "../../data/data";
 import FilmCard from "../FilmCard/FilmCard";
 import "./FilmsList.css";
 
@@ -5,16 +6,7 @@ const FilmsList = (): React.ReactElement => {
   return (
     <ul className="films">
       <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
-      </li>
-      <li>
-        <FilmCard />
+        <FilmCard film={filmsData[0]} />
       </li>
     </ul>
   );
