83372dc120f3683063011de139327659b11d68d3
Andrea Prat <andr8ide@gmail.com>
2023-08-15 13:50:13 +0200

    Add apiFilms in the FilmsContextProvider and loadFilms in FimsListPage

--- src/components/store/films/context/FilmsContextProvider.tsx
+++ src/components/store/films/context/FilmsContextProvider.tsx
@@ -1,13 +1,31 @@
-import React, { PropsWithChildren, useState } from "react";
-import { Film } from "../../../../types";
+import { PropsWithChildren, useCallback, useMemo, useState } from "react";
+import useFilmsApi from "../../../../hooks/useFilmsApi";
+import { FilmStructure } from "../../../../types";
 import FilmsContext from "./FilmContext";
+import { FilmContextStructure } from "./types";
 
 export const FilmsContextProvider = ({
   children,
 }: PropsWithChildren): React.ReactElement => {
-  const [films] = useState<Film[]>([]);
+  const [films, setFilms] = useState<FilmStructure[]>([]);
+  const { getFilms } = useFilmsApi();
+
+  const loadFilms = useCallback(async () => {
+    const apiFilms = await getFilms();
+    setFilms([...apiFilms]);
+  }, [getFilms]);
+
+  const filmContextValue = useMemo(
+    (): FilmContextStructure => ({
+      films,
+      loadFilms,
+    }),
+    [films, loadFilms],
+  );
 
   return (
-    <FilmsContext.Provider value={{ films }}>{children}</FilmsContext.Provider>
+    <FilmsContext.Provider value={filmContextValue}>
+      {children}
+    </FilmsContext.Provider>
   );
 };
