c8f687c0864a1a234935955145332ceed477a664
Andrea Prat <andr8ide@gmail.com>
2023-08-15 16:04:07 +0200

    Create the films list with .map()

--- src/components/FilmsList/FilmsList.tsx
+++ src/components/FilmsList/FilmsList.tsx
@@ -1,13 +1,18 @@
-import filmsData from "../../data/data";
+import React, { useContext } from "react";
 import FilmCard from "../FilmCard/FilmCard";
+import FilmsContext from "../store/films/context/FilmContext";
 import "./FilmsList.css";
 
 const FilmsList = (): React.ReactElement => {
+  const { films } = useContext(FilmsContext);
+
   return (
     <ul className="films">
-      <li>
-        <FilmCard film={filmsData[0]} />
-      </li>
+      {films.map((films) => (
+        <li key={films.id}>
+          <FilmCard film={films} />
+        </li>
+      ))}
     </ul>
   );
 };
